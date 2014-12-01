6145b24a41827f296df43a592ddb2e92bbac02f3
HIttWeb <gabriel@hittweb.com.br>
2014-12-01 01:27:50 -0300

    App saúde
    alteração no css e nova tela de "por dentro do tratamento"

--- por-dentro-do-tratamento.php
+++ por-dentro-do-tratamento.php
@@ -8,6 +8,66 @@
 		<img src="./images/topo-interna-por-dentro-do-tratamento.png" alt="">
 		<h2>Por dentro do<br>tratamento</h2>
 	</div>
-</div>
+
+		<div class="wrap-content tratamento">
+		<div class="wrap-abas">
+			<ul class="tabs">
+				<li>
+					<a id="sus" class="tab txt-normal" href="javascript:void(0);">O papel do SUS</a>
+				</li>
+				<li>
+					<a id="direitos" class="tab txt-normal" href="javascript:void(0);">Evolução do tratamento nos últimos 30 anos</a>
+				</li>
+				<li>
+					<a id="deu-positivo" class="tab txt-normal ativo" href="javascript:void(0);">Avanços <br>previstos</a>
+				</li>
+			</ul>
+			<div class="tabs-content-container">
+				<div id="sus" class="tab-content">
+					Sem conteúdo..
+				</div>
+				<div id="direitos" class="tab-content">
+					<p class="marginBottomP">Consequências do abandono do tratamento</p>
+
+					<p class="tit-maiorMuseu">Infecção aguda (período de 3 a 6 semanas)</p>
+					<ul class="MarginUl">
+						<li>Semelhantes à gripe</li>
+						<li>Febre</li>
+						<li>Mal estar</li>
+					</ul>
+
+					<p class="tit-maiorMuseu">Período assintomático (pode durar muitos anos)</p>
+					<ul class="MarginUl">
+						<li>Rápida mutação do vírus</li>
+						<li>Sistema imunológico pouco enfraquecido</li>
+						<li>Suscetível às Infecções comuns</li>
+					</ul>
+
+					<p class="tit-maiorMuseu">Fase Sintomática Inicial</p>
+					<ul class="MarginUl">
+						<li>Alta redução dos linfócitos. Sistema imunológico vulnerável às infecções comuns.</li>
+						<li>Febre</li>
+						<li>Diarréia</li>
+						<li>Suores noturnos</li>						
+						<li>Emagrecimento</li>						
+					</ul>
+
+					<p class="tit-maiorMuseu">A AIDS: Estágio mais avançado da doença </p>
+					<ul class="MarginUl">
+						<li>Aparecimento de doenças oportunistas que se aproveitam da baixa imunidade do corpo, como gripes, resfriados, pneumonia, infecções por fungos, diarreia crônica (mais de 30 dias), tuberculose...</li>
+						<li>As doenças oportunistas atingem o indivíduo com muito mais força do que atinge um indivíduo saudável.</li>										
+					</ul>
+
+
+				</div><!-- end tab -->
+
+				<div id="deu-positivo" class="tab-content ativo">
+					Sem conteúdo..
+				</div>
+			</div>
+		</div>
+	</div><!-- end wrap-content -->
+
+</div><!-- end conteudo-interno -->
 
 <?php include('./inc/footer.php'); ?>
