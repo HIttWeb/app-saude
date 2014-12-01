a1cd99d5bd27322d6e9824f0d9c607fb65404bf7
HIttWeb <gabriel@hittweb.com.br>
2014-12-01 02:56:54 -0300

    App Saúde
    Ajustes de css e nova tela "Sexualidade" estilo vertical das abas

--- sexualidade.doc.php
+++ sexualidade.doc.php
@@ -6,8 +6,96 @@
 	<?php include('./inc/breadcrumb.php'); ?>
 	<div class="topo-interna">
 		<img src="./images/topo-interna-sexualidade.png" alt="">
-		<h2>Sexualidade.doc</h2>
+		<h2 class="yellowTit">Sexualidade.doc</h2>
 	</div>
-</div>
+
+
+		<div class="wrap-content tratamento">
+		<div class="wrap-abas">
+			<ul class="tabs tabVertical">							
+				<li>
+					<a id="direitos" class="tab txt-normal" href="javascript:void(0);">Filmes</a>
+				</li>
+				<li>
+					<a id="deu-positivo" class="tab txt-normal" href="javascript:void(0);">Documentários</a>
+				</li>				
+				<li>
+					<a id="cuidados" class="tab txt-normal" href="javascript:void(0);">Livros</a>
+				</li>
+				<li>
+					<a id="sus" class="tab txt-normal ativo" href="javascript:void(0);">Artigos e pesquisas</a>
+				</li>
+			</ul>
+			<div class="tabs-content-container verticalTabContainer">
+							
+				<div id="direitos" class="tab-content">
+					Sem conteúdo..
+
+				</div>
+				<div id="deu-positivo" class="tab-content">
+					Sem conteúdo..
+				</div>
+				<div id="cuidados" class="tab-content">
+					Sem conteúdo..
+
+				</div><!-- end tab -->
+				
+				<div id="sus" class="tab-content ativo">
+					
+					<ul id="listaFilmes">
+						<li>
+							<div class="wrap-coverFilme">
+								<img src="./images/cover-filme.jpg" alt="">
+							</div>
+							<h5 class="titFilme">sexualidade sem censura</h5>
+							<p class="txt-descricao">Dicas, conteúdos  e indicações para você conhecer 
+							 tudo sobre HIV. Dicas, conteúdos  e indicações para você conhecer  tudo sobre HIV.  </p>
+						</li>
+						
+						<li>
+							<div class="wrap-coverFilme">
+								<img src="./images/cover-filme.jpg" alt="">
+							</div>
+							<h5 class="titFilme">sexualidade sem censura</h5>
+							<p class="txt-descricao">Dicas, conteúdos  e indicações para você conhecer 
+							 tudo sobre HIV. Dicas, conteúdos  e indicações para você conhecer  tudo sobre HIV.  </p>
+						</li>
+						<li>
+							<div class="wrap-coverFilme">
+								<img src="./images/cover-filme.jpg" alt="">
+							</div>
+							<h5 class="titFilme">sexualidade sem censura</h5>
+							<p class="txt-descricao">Dicas, conteúdos  e indicações para você conhecer 
+							 tudo sobre HIV. Dicas, conteúdos  e indicações para você conhecer  tudo sobre HIV.  </p>
+						</li>
+						<li>
+							<div class="wrap-coverFilme">
+								<img src="./images/cover-filme.jpg" alt="">
+							</div>
+							<h5 class="titFilme">sexualidade sem censura</h5>
+							<p class="txt-descricao">Dicas, conteúdos  e indicações para você conhecer 
+							 tudo sobre HIV. Dicas, conteúdos  e indicações para você conhecer  tudo sobre HIV.  </p>
+						</li>
+						<li>
+							<div class="wrap-coverFilme">
+								<img src="./images/cover-filme.jpg" alt="">
+							</div>
+							<h5 class="titFilme">sexualidade sem censura</h5>
+							<p class="txt-descricao">Dicas, conteúdos  e indicações para você conhecer 
+							 tudo sobre HIV. Dicas, conteúdos  e indicações para você conhecer  tudo sobre HIV.  </p>
+						</li>
+
+					</ul>
+
+					
+
+				</div>
+			</div>
+		</div>
+	</div><!-- end wrap-content -->
+
+
+
+</div><!-- end conteudo-interno -->
 
 <?php include('./inc/footer.php'); ?>
